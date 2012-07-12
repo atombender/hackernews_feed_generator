5fa1f9f4166bb546cc9f68593caf90dcdeb97ff0
Alexander Staubo <alex@origo.no>
2012-07-12 15:16:38 +0200

    Log to stdout.

--- lib/hackernewsfeed/common.rb
+++ lib/hackernewsfeed/common.rb
@@ -5,7 +5,7 @@ module Hackernewsfeed
   USER_AGENT = "hackernews_feed_generator/#{APP_VERSION} (+https://github.com/alexstaubo/hackernews_feed_generator)".freeze
 
   def self.logger
-    @logger ||= Logger.new($stderr)
+    @logger ||= Logger.new($stdout)
   end
 
 end
